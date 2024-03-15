eb277a911c8d0c4c6e6bfd8e1ec90a085d5c2c15
Julien Bailly <julien.bailly2@getu.univ-orleans.fr>
2024-03-15 15:20:47 +0100

    debut Plateau.java et CaseIntelligence.java

--- src/Plateau.java
+++ src/Plateau.java
@@ -3,7 +3,34 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Plateau{
+    private int nbLignes;
+    private int nbColonnes;
+    private int pourcentageDeBombes;
+    private int nbBombes;
+    private List<lePlateau> lesCases;
 
+    public Plateau(int nbLignes, int nbColonnes, int pourcentageDeBombes){
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+        this.pourcentageDeBombes = pourcentageDeBombes;
+        this.nbBombes = 0;
+    }
+
+    private void creerLesCasesVides(){
+        for (int x = 0; x < this.getNbLignes(); x++){
+            for (int y = 0; y < this.getNbColonnes(); y++){
+                ;
+            }
+        }
+    }
+
+    private void rendLesCasesIntelligentes(){
+        for (int x = 0; x < this.getNbLignes(); x++){
+            for (int y = 0; y < this.getNbColonnes(); y++){
+                ;
+            }
+        }
+    }
 
     protected void poseDesBombesAleatoirement(){
         Random generateur = new Random();
@@ -16,5 +43,6 @@ public class Plateau{
             }
         }
     }
+
 
 }
