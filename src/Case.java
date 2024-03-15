20d6a99c87cf55bbf11a52a1b7a4974fa1a0f7e3
Julien Bailly <julien.bailly2@getu.univ-orleans.fr>
2024-03-15 14:53:20 +0100

    Case.java

--- src/Case.java
+++ src/Case.java
@@ -1,3 +1,41 @@
 public class Case {
+    private boolean contientUneBombe;
+    private boolean estDecouverte;
+    private boolean estMarquee;
     
+    public Case() {
+        this.contientUneBombe = false;
+        this.estDecouverte = false;
+        this.estMarquee = false;
+    }
+
+    public void reset(){
+        this.contientUneBombe = false;
+        this.estDecouverte = false;
+        this.estMarquee = false;
+    }
+
+    public void poseBombe(){
+        this.contientUneBombe = true;
+    }
+
+    public contientUneBombe(){
+        return this.contientUneBombe;
+    }
+
+    public boolean estDecouverte(){
+        return this.estDecouverte;
+    }
+
+    public boolean estMarquee(){
+        return this.estMarquee;
+    }
+
+    public reveler(){
+        this.estMarquee = true;
+    }
+
+    public marquer(){
+        this.estMarquee = true;
+    }
 }
